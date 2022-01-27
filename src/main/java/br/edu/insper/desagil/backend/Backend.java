5188e05e497a2c1c3c8d60d07b5905e424201f11
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-01-27 07:51:38 -0300

    Refactor backend

--- src/main/java/br/edu/insper/desagil/backend/Backend.java
+++ src/main/java/br/edu/insper/desagil/backend/Backend.java
@@ -8,17 +8,18 @@ import br.pro.hashi.nfp.rest.server.RestServer;
 public class Backend {
 	public static void main(String[] args) {
 		Factory factory = new Factory("main");
+
 		Firebase firebase = factory.createFirebase();
+		firebase.connect();
+
 		RestServer server = factory.createRestServer();
 		boolean useTunnel = factory.useTunnel();
-
-		firebase.connect();
 		server.start(useTunnel);
 
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 			server.stop();
-			firebase.disconnect();
 			server.destroy();
+			firebase.disconnect();
 			firebase.remove();
 		}));
 	}
